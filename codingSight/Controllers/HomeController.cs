b724c9c6d1f51816e8e33d7b924e1ab2c1b72743
ZornitsaSerbezova <pesheva@gmail.com>
2020-06-05 15:52:30 +0300

    first commit

---
+++ codingSight/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using codingSight.Models;
+
+namespace codingSight.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+            => View();
+
+        public string Error()
+           => "Error";
+    }
+}
