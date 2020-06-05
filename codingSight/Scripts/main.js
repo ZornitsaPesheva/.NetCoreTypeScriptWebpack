a69955b14d100fb8a5b046e6df29f8ae630b6a32
ZornitsaSerbezova <pesheva@gmail.com>
2020-06-05 18:50:45 +0300

    orgchart added

--- codingSight/Scripts/main.js
+++ codingSight/Scripts/main.js
@@ -1,1 +1,13 @@
-﻿document.getElementById("helloworld").innerText = "Hello world from script";
+﻿//document.getElementById("helloworld").innerText = "Hello world from script";
+import OrgChart from "@balkangraph/orgchart.js";
+
+var chart = new OrgChart(document.getElementById("orgchart"), {
+    nodeBinding: {
+        field_0: "name"
+    },
+    nodes: [
+        { id: 1, name: "Amber McKenzie" },
+        { id: 2, pid: 1, name: "Ava Field" },
+        { id: 3, pid: 1, name: "Peter Stevens" }
+    ]
+});
